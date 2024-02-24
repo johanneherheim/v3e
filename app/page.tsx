6b7ee6b34f0ff08d4fc0bb5c42d764e4fa9d84c3
Johanne Blikberg Herheim <joher2128@uib.no>
2024-02-24 19:07:31 +0100

    fixed nynorsk

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <div className="absolute inset-0"></div>
       </div>
       <div className="absolute bg-white bg-opacity-70 p-4 sm:p-5 rounded top-40 sm:top-24">
-        <h1 className="text-center text-2xl ">Velkommen til Voss 3-etappers</h1>
+        <h1 className="text-center text-2xl ">Velkommen til Voss 3-etappars</h1>
         <h2 className="antialiased leading-relaxed font-extrabold tracking-widest text-2xl sm:text-7xl text-center px-5 ">
           3. – 4. august 2024
         </h2>
