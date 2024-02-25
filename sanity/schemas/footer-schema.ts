5a820dc87b1a7d9b4c1f1ec98218a015825d3c80
Johanne Blikberg Herheim <joher2128@uib.no>
2024-02-25 12:16:56 +0100

    added footer schema

---
+++ sanity/schemas/footer-schema.ts
@@ -0,0 +1,29 @@
+const footer = {
+  name: "footer",
+  title: "Footer",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+  ],
+};
+
+export default footer;
