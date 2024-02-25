5b86ab1e5d5560d8e8ed4f9c398a7f321366bc97
Johanne Blikberg Herheim <joher2128@uib.no>
2024-02-25 12:33:33 +0100

    minimum height

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -9,7 +9,7 @@ export default async function Page({ params }: Props) {
   const page = await getPage(params.slug);
   return (
     <div className="px-10 py-20 max-w-5xl mx-auto">
-      <h1 className="text-4xl py-2 drop-shadow font-bold ">{page.title}</h1>
+      <h1 className="text-4xl py-2 font-bold min-h-screen">{page.title}</h1>
       <div className="text-lg text-gray-700 mt-5">
         <PortableText value={page.content} />
       </div>
