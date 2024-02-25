999b8d2aa3892b48c29047fc06953bffd36bcb64
Johanne Blikberg Herheim <joher2128@uib.no>
2024-02-25 11:25:32 +0100

    made page prettier

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -8,10 +8,8 @@ type Props = {
 export default async function Page({ params }: Props) {
   const page = await getPage(params.slug);
   return (
-    <div className="p-20">
-      <h1 className="text-4xl py-2 drop-shadow font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-purple-600 via-red-500">
-        {page.title}
-      </h1>
+    <div className="px-10 py-20 max-w-5xl mx-auto">
+      <h1 className="text-4xl py-2 drop-shadow font-bold ">{page.title}</h1>
       <div className="text-lg text-gray-700 mt-5">
         <PortableText value={page.content} />
       </div>
