2ce9bc53e1d987a473483eaa932e8e801e23193b
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-03 17:28:38 +0100

    testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚲</text></svg>"
-      />
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚲</text></svg>"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#325758" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
