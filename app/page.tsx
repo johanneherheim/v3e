71c77f5f3a2674de515c8bffcc44fc391b046740
Johanne Blikberg Herheim <joher2128@uib.no>
2024-06-26 22:59:26 +0200

    added button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import hero from "@/components/images/IMG_0250.jpeg";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   const viewportHeight = `calc(100vh - ${64}px)`;
   return (
-    <div className="relative flex items-center justify-center h-2/3">
+    <div className="relative flex flex-col items-center justify-center h-2/3">
       <div className="relative w-full h-full">
         <Image
           src={hero}
@@ -19,12 +21,20 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="absolute bg-white bg-opacity-70 p-4 sm:p-5 rounded top-40 sm:top-24">
-        <h2 className="text-center  ">Velkommen til Voss 3-etappars</h2>
+      <div className="absolute top-1/4 bg-white bg-opacity-70 p-4 sm:p-5 rounded">
+        <h2 className="text-center">Velkommen til Voss 3-etappars</h2>
         <h1 className="antialiased leading-relaxed font-extrabold tracking-widest text-2xl sm:text-7xl text-center px-5">
           3. – 4. august 2024
         </h1>
       </div>
+      <Link
+        href="https://live.eqtiming.com/71975#dashboard"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="absolute bottom-32 sm:bottom-1/6 flex justify-center bg-black text-white rounded-lg py-5 px-10 text-xl font-semibold"
+      >
+        Meld deg på her
+      </Link>
     </div>
   );
 }
