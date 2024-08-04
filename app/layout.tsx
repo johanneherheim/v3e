ae3e3a8ad05b1b23fd7b96249a18fbc9bc6efa31
Johanne Blikberg Herheim <joher2128@uib.no>
2024-08-04 20:01:09 +0200

    Meta status bar style

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
-          content="black-translusent"
+          content="black-translucent"
         />
         <meta
           name="description"
